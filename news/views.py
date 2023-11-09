02d8e8cdc72880cf8a751decca866f973e5977f6
Name <email@example.ru>
2023-11-09 18:09:54 +0500

    Init project modD5.8

---
+++ news/views.py
@@ -0,0 +1,115 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.urls import reverse_lazy
+from datetime import datetime
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+
+from .filters import PostFilter
+from .forms import PostForm
+from .models import Post
+
+class PostList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    # model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    # ordering = '-time_creates'
+    queryset = Post.objects.filter(
+        choice_content='AR'
+    )
+    template_name = 'posts.html'
+    context_object_name = 'headings'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
+
+class NewsList(ListView):
+    queryset = Post.objects.filter(
+        choice_content='NW'
+    )
+    template_name = 'news.html'
+    context_object_name = 'headings'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        # context['autor'] = Post.autor
+        return context
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'text_posts.html'
+    context_object_name = 'heading'
+
+class NewsDetail(DetailView):
+    model = Post
+    template_name = 'text_news.html'
+    context_object_name = 'heading'
+
+class PostCreate(PermissionRequiredMixin, CreateView):
+    permission_required = ('news.add_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'posts_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.choice_content='AR'
+        return super().form_valid(form)
+
+class NewsCreate(PermissionRequiredMixin, CreateView):
+    permission_required = ('news.add_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.choice_content='NW'
+        return super().form_valid(form)
+
+class PostEdit(PermissionRequiredMixin, UpdateView):
+    permission_required = ('news.change_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'posts_edit.html'
+
+class NewsEdit(PermissionRequiredMixin, UpdateView):
+    permission_required = ('news.change_post',)
+    form_class = PostForm
+    model = Post
+    template_name = 'news_edit.html'
+
+class PostDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = ('news.delete_post',)
+    model = Post
+    template_name = 'posts_delete.html'
+    success_url = reverse_lazy('post_list')
+
+class NewsDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = ('news.delete_post',)
+    model = Post
+    template_name = 'news_delete.html'
+    success_url = reverse_lazy('news_list')
+
+class NewsSearch(ListView):
+    model = Post
+    ordering = 'heading'
+    template_name = 'news_search.html'
+    context_object_name = 'news'
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
